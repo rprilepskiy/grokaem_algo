cd3d8d810d3845a56ac070cdceb8006e47817e2f
Roman Prilepskiy <r.prilepskiy@gmail.com>
2023-02-16 12:19:13 +0300

    Updated conftest.py - list_100(), list_1000000()

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,17 @@
 import pytest
+import numpy as np
+
 
 @pytest.fixture()
 def sorted_list():
     return [1, 3, 5, 7, 9]
+
+
+@pytest.fixture
+def list_100():
+    return np.arange(1, 101, 1)
+
+
+@pytest.fixture
+def list_1000000():
+    return np.arange(1, 1000001, 1)
