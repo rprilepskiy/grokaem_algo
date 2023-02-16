3478e8512bf47d54550fb8ffea600770e179dd34
Roman Prilepskiy <r.prilepskiy@gmail.com>
2023-02-16 11:15:54 +0300

    Fixed bugs

--- chapter_2/binary_search.py
+++ chapter_2/binary_search.py
@@ -3,8 +3,8 @@ def binary_search(list, item):
     high = len(list) - 1
 
     while low <= high:
-        mid = (low + high) / 2
-        guess = list(mid)
+        mid = (low + high) // 2
+        guess = list[mid]
         if guess == item:
             return guess
         elif guess > item:
