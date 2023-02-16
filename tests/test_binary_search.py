910cea8c32ac31dc6fda912b08ca038283dc5c2d
Roman Prilepskiy <r.prilepskiy@gmail.com>
2023-02-16 12:29:38 +0300

    Modified test_binary_search.py - added fixtures, parametrization

--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -1,14 +1,25 @@
+import pytest
 from chapter_2.binary_search import binary_search
 
-def test_binary_search():
+@pytest.mark.parametrize("item, expected", [
+    (3, 3),
+    (2, None),
+    (5, 5),
+    (7, 7),
+    (9, 9),
+    (0, None),
+    (10, None),
+    (-1, None)
+])
+def test_binary_search(sorted_list, item, expected):
     # Test a case where the item is present in the list
-    assert binary_search([1, 3, 5, 7, 9], 3) == 3
+    assert binary_search(sorted_list, item) == expected
 
-    # Test a case where the item is not present in the list
-    assert binary_search([1, 3, 5, 7, 9], 2) is None
+@pytest.mark.parametrize("item", [1, 50, 100])
+def test_binary_search_100(list_100, item):
+    assert binary_search(list_100, item) == item
 
-    # Test a case where the list is empty
-    assert binary_search([], 1) is None
 
-    # Test a case where the list has only one element
-    assert binary_search([5], 5) == 5
+@pytest.mark.parametrize("item", [1, 500000, 1000000])
+def test_binary_seach_1000000(list_1000000, item):
+    assert binary_search(list_1000000, item) == item
